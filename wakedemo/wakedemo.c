01c9f557eb643803bd586c79d28eb88a82934c70
TheeHP3 <hpedroza@miners.utep.edu>
2026-07-28 12:09:23 -0600

    Modified wakedemo.c to have the start screen and the ability to move to question 1

--- wakedemo/wakedemo.c
+++ wakedemo/wakedemo.c
@@ -3,176 +3,246 @@
 #include "lcdutils.h"
 #include "lcddraw.h"
 
-// WARNING: LCD DISPLAY USES P1.0.  Do not touch!!! 
+/* --------------------------------------------------------- */
+/*                  DO NOT TOUCH P1.0 (LCD)                  */
+/* --------------------------------------------------------- */
 
-#define LED BIT6		/* note that bit zero req'd for display */
+#define LED BIT6
 
 #define SW1 1
 #define SW2 2
 #define SW3 4
 #define SW4 8
 
-#define SWITCHES 15
+#define SWITCHES (SW1 | SW2 | SW3 | SW4)
 
-char blue = 31, green = 0, red = 31;
-unsigned char step = 0;
-
-static char 
-switch_update_interrupt_sense()
-{
-  char p2val = P2IN;
-  /* update switch interrupt to detect changes from current buttons */
-  P2IES |= (p2val & SWITCHES);	/* if switch up, sense down */
-  P2IES &= (p2val | ~SWITCHES);	/* if switch down, sense up */
-  return p2val;
-}
-
-void 
-switch_init()			/* setup switch */
-{  
-  P2REN |= SWITCHES;		/* enables resistors for switches */
-  P2IE |= SWITCHES;		/* enable interrupts from switches */
-  P2OUT |= SWITCHES;		/* pull-ups for switches */
-  P2DIR &= ~SWITCHES;		/* set switches' bits for input */
-  switch_update_interrupt_sense();
-}
+/* --------------------------------------------------------- */
+/*                  Game Variables                           */
+/* --------------------------------------------------------- */
 
 int switches = 0;
 
-void
-switch_interrupt_handler()
+char redrawScreen = 1;
+
+int gameStarted = 0;
+
+int currentQuestion = 0;
+
+int lives = 3;
+
+/* --------------------------------------------------------- */
+/*                  Question Data                            */
+/* --------------------------------------------------------- */
+
+char *questions[] = {
+    "Question 1",
+    "Question 2",
+    "Question 3"
+};
+
+char *questionInfo[] = {
+    "The answer is 1",
+    "The answer is 1",
+    "The answer is 1"
+};
+
+char *answers[][4] = {
+    {"S1: Answer 1",
+     "S2: Answer 2",
+     "S3: Answer 3",
+     "S4: Answer 4"},
+
+    {"S1: Answer 1",
+     "S2: Answer 2",
+     "S3: Answer 3",
+     "S4: Answer 4"},
+
+    {"S1: Answer 1",
+     "S2: Answer 2",
+     "S3: Answer 3",
+     "S4: Answer 4"}
+};
+
+/* --------------------------------------------------------- */
+/*                  Switch Code                              */
+/* --------------------------------------------------------- */
+
+static char switch_update_interrupt_sense()
 {
-  char p2val = switch_update_interrupt_sense();
-  switches = ~p2val & SWITCHES;
+    char p2val = P2IN;
+
+    P2IES |= (p2val & SWITCHES);
+    P2IES &= (p2val | ~SWITCHES);
+
+    return p2val;
 }
 
-
-// axis zero for col, axis 1 for row
-
-short drawPos[2] = {1,10}, controlPos[2] = {2, 10};
-short colVelocity = 1, colLimits[2] = {1, screenWidth/2};
-
-void
-draw_ball(int col, int row, unsigned short color)
+void switch_init()
 {
-  fillRectangle(col-1, row-1, 3, 3, color);
+    P2REN |= SWITCHES;
+    P2IE |= SWITCHES;
+    P2OUT |= SWITCHES;
+    P2DIR &= ~SWITCHES;
+
+    switch_update_interrupt_sense();
 }
 
-
-void
-screen_update_ball()
+void switch_interrupt_handler()
 {
-  for (char axis = 0; axis < 2; axis ++) 
-    if (drawPos[axis] != controlPos[axis]) /* position changed? */
-      goto redraw;
-  return;			/* nothing to do */
- redraw:
-  draw_ball(drawPos[0], drawPos[1], COLOR_BLUE); /* erase */
-  for (char axis = 0; axis < 2; axis ++) 
-    drawPos[axis] = controlPos[axis];
-  draw_ball(drawPos[0], drawPos[1], COLOR_WHITE); /* draw */
-}
-  
+    char p2val = switch_update_interrupt_sense();
 
-short redrawScreen = 1;
-u_int controlFontColor = COLOR_GREEN;
+    switches = ~p2val & SWITCHES;
+
+    /* Start game on first button press */
+
+    if (!gameStarted && switches)
+    {
+        gameStarted = 1;
+        redrawScreen = 1;
+    }
+}
+
+/* --------------------------------------------------------- */
+/*                  LCD Drawing                              */
+/* --------------------------------------------------------- */
+
+void drawStartScreen()
+{
+    clearScreen(COLOR_BLUE);
+
+    drawString5x7(20,20,
+                  "QUIZ GAME",
+                  COLOR_WHITE,
+                  COLOR_BLUE);
+
+    drawString5x7(8,50,
+                  "Press Any Button",
+                  COLOR_WHITE,
+                  COLOR_BLUE);
+}
+
+void drawQuestion()
+{
+    char livesText[10];
+
+    clearScreen(COLOR_BLACK);
+
+    livesText[0] = 'L';
+    livesText[1] = 'i';
+    livesText[2] = 'v';
+    livesText[3] = 'e';
+    livesText[4] = 's';
+    livesText[5] = ':';
+    livesText[6] = ' ';
+    livesText[7] = '0' + lives;
+    livesText[8] = '\0';
+
+    drawString5x7(5,
+                  5,
+                  livesText,
+                  COLOR_GREEN,
+                  COLOR_BLACK);
+
+    drawString5x7(5,
+                  25,
+                  questions[currentQuestion],
+                  COLOR_WHITE,
+                  COLOR_BLACK);
+
+    drawString5x7(5,
+                  40,
+                  questionInfo[currentQuestion],
+                  COLOR_YELLOW,
+                  COLOR_BLACK);
+
+    drawString5x7(5,
+                  70,
+                  answers[currentQuestion][0],
+                  COLOR_WHITE,
+                  COLOR_BLACK);
+
+    drawString5x7(5,
+                  85,
+                  answers[currentQuestion][1],
+                  COLOR_WHITE,
+                  COLOR_BLACK);
+
+    drawString5x7(5,
+                  100,
+                  answers[currentQuestion][2],
+                  COLOR_WHITE,
+                  COLOR_BLACK);
+
+    drawString5x7(5,
+                  115,
+                  answers[currentQuestion][3],
+                  COLOR_WHITE,
+                  COLOR_BLACK);
+}
+
+void redrawGame()
+{
+    if (!gameStarted)
+        drawStartScreen();
+    else
+        drawQuestion();
+}
+
+/* --------------------------------------------------------- */
+/*          Watchdog Timer Interrupt Handler                 */
+/* --------------------------------------------------------- */
 
 void wdt_c_handler()
 {
-  static int secCount = 0;
-
-  secCount ++;
-  if (secCount >= 25) {		/* 10/sec */
-   
-    {				/* move ball */
-      short oldCol = controlPos[0];
-      short newCol = oldCol + colVelocity;
-      if (newCol <= colLimits[0] || newCol >= colLimits[1])
-	colVelocity = -colVelocity;
-      else
-	controlPos[0] = newCol;
-    }
-
-    {				/* update hourglass */
-      if (switches & SW3) green = (green + 1) % 64;
-      if (switches & SW2) blue = (blue + 2) % 32;
-      if (switches & SW1) red = (red - 3) % 32;
-      if (step <= 30)
-	step ++;
-      else
-	step = 0;
-      secCount = 0;
-    }
-    if (switches & SW4) return;
-    redrawScreen = 1;
-  }
+    /* Nothing timed yet.
+       We simply keep the framework your professor provided. */
 }
-  
-void update_shape();
+
+/* --------------------------------------------------------- */
+/*                      Main                                 */
+/* --------------------------------------------------------- */
 
 void main()
 {
-  
-  P1DIR |= LED;		/**< Green led on when CPU on */
-  P1OUT |= LED;
-  configureClocks();
-  lcd_init();
-  switch_init();
-  
-  enableWDTInterrupts();      /**< enable periodic interrupt */
-  or_sr(0x8);	              /**< GIE (enable interrupts) */
-  
-  clearScreen(COLOR_BLUE);
-  while (1) {			/* forever */
-    if (redrawScreen) {
-      redrawScreen = 0;
-      update_shape();
+    P1DIR |= LED;
+    P1OUT |= LED;
+
+    configureClocks();
+
+    lcd_init();
+
+    switch_init();
+
+    enableWDTInterrupts();
+
+    or_sr(0x8);
+
+    while (1)
+    {
+        if (redrawScreen)
+        {
+            redrawScreen = 0;
+            redrawGame();
+        }
+
+        P1OUT &= ~LED;
+
+        or_sr(0x10);
+
+        P1OUT |= LED;
     }
-    P1OUT &= ~LED;	/* led off */
-    or_sr(0x10);	/**< CPU OFF */
-    P1OUT |= LED;	/* led on */
-  }
 }
 
-void
-screen_update_hourglass()
+/* --------------------------------------------------------- */
+/*             Port 2 Interrupt                              */
+/* --------------------------------------------------------- */
+
+void __interrupt_vec(PORT2_VECTOR) Port_2()
 {
-  static unsigned char row = screenHeight / 2, col = screenWidth / 2;
-  static char lastStep = 0;
-  
-  if (step == 0 || (lastStep > step)) {
-    clearScreen(COLOR_BLUE);
-    lastStep = 0;
-  } else {
-    for (; lastStep <= step; lastStep++) {
-      int startCol = col - lastStep;
-      int endCol = col + lastStep;
-      int width = 1 + endCol - startCol;
-      
-      // a color in this BGR encoding is BBBB BGGG GGGR RRRR
-      unsigned int color = (blue << 11) | (green << 5) | red;
-      
-      fillRectangle(startCol, row+lastStep, width, 1, color);
-      fillRectangle(startCol, row-lastStep, width, 1, color);
+    if (P2IFG & SWITCHES)
+    {
+        P2IFG &= ~SWITCHES;
+
+        switch_interrupt_handler();
     }
-  }
-}  
-
-
-    
-void
-update_shape()
-{
-  screen_update_ball();
-  screen_update_hourglass();
-}
-   
-
-
-void
-__interrupt_vec(PORT2_VECTOR) Port_2(){
-  if (P2IFG & SWITCHES) {	      /* did a button cause this interrupt? */
-    P2IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
-    switch_interrupt_handler();	/* single handler for all switches */
-  }
 }
